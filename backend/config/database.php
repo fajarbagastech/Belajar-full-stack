f360a56ff22ebf925a4c106023a98a449de4ecc0
fajarbagasp <bagasf576@gmail.com>
2026-04-17 01:11:08 +0700

    fix conection db2

--- backend/config/database.php
+++ backend/config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'database' => env('DB_DATABASE', 'railway'),
+    'database' => env('DB_DATABASE', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -47,7 +47,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'courageous-nourishment-production-21ca.up.railway.app'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'database' => env('DB_DATABASE', 'railway'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
