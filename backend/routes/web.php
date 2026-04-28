a48bd5c4754fbc485f53c8343c7990a6cb18380c
fajarbagasp <bagasf576@gmail.com>
2026-04-29 00:31:34 +0700

    Fix database host configuration4

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return "Koneksi Aman, Jar!";
+    } catch (\Exception $e) {
+        return "Masih Error: " . $e->getMessage();
+    }
 });
